c517e3a95efb86ee1bb7ea1ecc19c741d1872589
dojiyoung <84650928+dojiyoung@users.noreply.github.com>
2022-08-20 15:59:46 +1200

    dsadsdsdsa

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,19 +1,28 @@
 import { useState } from "react";
 import useSWR from "swr";
+import { Liquid } from '@ant-design/plots';
 
 export const Dashboard = ({ id }) => {
   const { data } = useSWR("", { refreshInterval: 1 });
 
   // if (!id) return;
-
+  const config = {
+    percent: 0.25,
+    shape: 'rect',
+    outline: {
+      border: 2,
+      distance: 4,
+    },
+    wave: {
+      length: 128,
+    },
+  };
   return (
     <div className="widget-grid">
-      <div className="widget">{id}</div>
-      <div className="widget">{id}</div>
-      <div className="widget">{id}</div>
-      <div className="widget">{id}</div>
-      <div className="widget">{id}</div>
-      <div className="widget">{id}</div>
+      <Liquid {...config} />
+
     </div>
   );
 };
+
+
