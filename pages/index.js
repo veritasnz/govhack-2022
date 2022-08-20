0785b5c2772a95b89cd1b111525e9bc9a6c70a7b
dojiyoung <84650928+dojiyoung@users.noreply.github.com>
2022-08-20 16:54:49 +1200

    dadda

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Head from "next/head";
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import { Layout } from "antd";
 
 import { Map } from "../src/components/Map";
-import { Menu } from "../src/components/Menu";
+// import { Menu } from "../src/components/Menu";
 import { CCTV } from "../src/components/CCTV";
 import { Dashboard } from "../src/components/Dashboard";
+
+const { Sider, Menu } = Layout;
 
 export default function Home() {
   return (
@@ -14,8 +17,10 @@ export default function Home() {
         <meta name="description" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main>
+        <Sider>
+          <Menu theme="dark" mode="inline" items={[]} />
+        </Sider>
         <Menu />
         <CCTV />
         <Map />
