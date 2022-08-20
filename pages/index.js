e3db4a6e6d478825571003abbb9be7e337114355
veritasnz <7684225+veritasnz@users.noreply.github.com>
2022-08-20 15:36:51 +1200

    integrate new drawer

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,21 @@
 import Head from "next/head";
-import { useState } from "react";
-import { Button } from 'antd';
 import { Map } from "../src/components/Map";
 import { Pullout } from "../src/components/Pullout";
-import { PipeDrawer } from "../src/components/PipeDrawer";
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 
 export default function Home() {
-    const [showDrawer, setShowDrawer] = useState(false);
+  return (
+    <div>
+      <Head>
+        <title>PipeWatch</title>
+        <meta name="description" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-    function pipeClicked() {
-        setShowDrawer(true);
-    }
-
-    function onClose() {
-        setShowDrawer(false);
-    }
-
-    return (
-        <div>
-            <Head>
-                <title>PipeWatch</title>
-                <meta name="description" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-
-            <main>
-                <Pullout />
-                <Map />
-                <Button type="primary" onClick={pipeClicked}>
-                    Open
-                </Button>
-                <PipeDrawer showDrawer={showDrawer} onClose={onClose} />
-            </main>
-        </div>
-    );
+      <main>
+        <Map />
+        <Pullout />
+      </main>
+    </div>
+  );
 }
