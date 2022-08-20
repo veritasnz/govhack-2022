9f1d7dac4c9c74380c0ab2226a30a59a25e80026
veritasnz <7684225+veritasnz@users.noreply.github.com>
2022-08-20 15:57:36 +1200

    commit

--- src/components/Map.js
+++ src/components/Map.js
@@ -6,13 +6,8 @@ import { MapComponent } from "./MapComponent";
 export const Map = (props) => {
   const pullout = useContext(PulloutContext);
 
-  const openHandler = () => {
-    pullout.open(1);
-  };
-
   return (
     <div>
-      <button onClick={openHandler}>Open</button>
       <div className="map">
         <Wrapper apiKey={process.env.NEXT_PUBLIC_MAPS_API_KEY}>
           <MapComponent />
