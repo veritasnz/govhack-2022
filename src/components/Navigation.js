7d9ad72f44e60ecb82980f0f58ca87f18e31bc4e
Lite W Kim <sd.94lite@gmail.com>
2022-08-20 20:46:04 +1200

    update menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,42 @@
 import React from "react";
 import { Layout, Menu } from "antd";
-import { AppstoreOutlined } from '@ant-design/icons';
+import { AppstoreOutlined, ControlOutlined, AreaChartOutlined, SettingOutlined } from '@ant-design/icons';
 
 const { Sider } = Layout;
 
+const ICON_STYLE = {
+  marginTop: "8px",
+  fontSize: "48px",
+};
+
+const LABEL_STYLE = {
+  position: "relative",
+  top: "-8px",
+  fontSize: "small",
+  height: "16px",
+  lineHeight: "16px"
+}
+
 export default function Navigation() {
   return (
-    <Sider width={64}>
+    <Sider width={80}>
       <Menu theme="dark" mode="inline">
-        <Menu.Item key="1">1</Menu.Item>
-        <Menu.Item key="2">2</Menu.Item>
-        <Menu.Item key="3">3</Menu.Item>
+        <Menu.Item key="1">
+          <AppstoreOutlined style={ICON_STYLE} />
+          <div style={LABEL_STYLE}>Dashboard</div>
+        </Menu.Item>
+        <Menu.Item key="2">
+          <ControlOutlined style={ICON_STYLE} />
+          <div style={LABEL_STYLE}>Actions</div>
+        </Menu.Item>
+        <Menu.Item key="3">
+          <AreaChartOutlined style={ICON_STYLE} />
+          <div style={LABEL_STYLE}>Metrics</div>
+        </Menu.Item>
+        <Menu.Item key="4">
+          <SettingOutlined style={ICON_STYLE} />
+          <div style={LABEL_STYLE}>Settings</div>
+        </Menu.Item>
       </Menu>
     </Sider>
   )
