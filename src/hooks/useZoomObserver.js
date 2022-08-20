2485e53abed682d3bd346c0518adcb113654288e
veritasnz <7684225+veritasnz@users.noreply.github.com>
2022-08-20 15:25:13 +1200

    commit

--- src/hooks/useZoomObserver.js
+++ src/hooks/useZoomObserver.js
@@ -1,3 +1,43 @@
+import { useEffect, useState } from "react";
+
 export const useZoomObserver = (map) => {
-  console.log(map);
+  const [isZoomed, setIsZoomed] = useState(false);
+  const [pipes, setPipes] = useState([]);
+
+  // Listen for zoom changes and adjust `isZoomed`
+  useEffect(() => {
+    if (!map) return;
+
+    google.maps.event.addListener(map, "zoom_changed", function () {
+      const zoom = map.getZoom();
+
+      setIsZoomed(zoom >= 15);
+    });
+  }, [map]);
+
+  useEffect(() => {
+    if (!map) return;
+
+    if (!isZoomed) {
+      setPipes([]);
+      return;
+    }
+
+    setPipes((prev) => {
+      const newPipes = [...prev];
+
+      newPipes.push({
+        id: 1,
+        startPoint: [-43.516508813449725, 172.60481119477708],
+        endPoint: [-43.542943375962224, 172.6611207508225],
+      });
+
+      return newPipes;
+    });
+  }, [isZoomed]);
+
+  return {
+    pipes,
+    isZoomed,
+  };
 };
